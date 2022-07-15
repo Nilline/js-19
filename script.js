7b73bd55ea83aa6dbcc3848cb92379ad7b284525
Eugene <nilline2017@gmail.com>
2022-07-15 09:48:01 +0300

    complete 21 lesson

--- script.js
+++ script.js
@@ -33,12 +33,58 @@ const personalMovieDB = {
 	privat: false
 };
 const lastFilmName1 = prompt('Один из последних просмотренных фильмов?', ''),
-		lastFilmRating1 = prompt('На сколько оцените его?', ''),
-		lastFilmName2 = prompt('Один из последних просмотренных фильмов?', ''),
-		lastFilmRating2 = prompt('На сколько оцените его?', '');
+	lastFilmRating1 = prompt('На сколько оцените его?', ''),
+	lastFilmName2 = prompt('Один из последних просмотренных фильмов?', ''),
+	lastFilmRating2 = prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[lastFilmName1] = lastFilmRating1;
 personalMovieDB.movies[lastFilmName2] = lastFilmRating2;
 
 
 console.log(personalMovieDB);
+
+
+/*
+console.log(NaN || 2 || undefined);
+
+console.log(NaN && 2 && undefined);
+
+console.log(1 && 2 && 3);
+
+console.log(!1 && 2 || !3);
+
+console.log(25 || null && !3);
+
+console.log(NaN || null || !3 || undefined || 5);
+
+console.log(NaN || null && !3 && undefined || 5);
+
+console.log(5 === 5 && 3 > 1 || 5);
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+	console.log('Done!')
+}
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger || cola || fries === 3 || nuggets) {
+	console.log('Done!')
+}
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger && cola || fries === 3 && nuggets) {
+	console.log('Done!')
+}
+*/

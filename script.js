271b1413fdc63e6a0661b9fcebb4bfd898774dac
Eugene <nilline2017@gmail.com>
2022-07-15 14:13:03 +0300

    complete 23 lesson prantice

--- script.js
+++ script.js
@@ -23,25 +23,25 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
-const lastFilmName1 = prompt('Один из последних просмотренных фильмов?', ''),
-	lastFilmRating1 = prompt('На сколько оцените его?', ''),
-	lastFilmName2 = prompt('Один из последних просмотренных фильмов?', ''),
-	lastFilmRating2 = prompt('На сколько оцените его?', '');
+// const personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false
+// };
+// const lastFilmName1 = prompt('Один из последних просмотренных фильмов?', ''),
+// 	lastFilmRating1 = prompt('На сколько оцените его?', ''),
+// 	lastFilmName2 = prompt('Один из последних просмотренных фильмов?', ''),
+// 	lastFilmRating2 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastFilmName1] = lastFilmRating1;
-personalMovieDB.movies[lastFilmName2] = lastFilmRating2;
+// personalMovieDB.movies[lastFilmName1] = lastFilmRating1;
+// personalMovieDB.movies[lastFilmName2] = lastFilmRating2;
 
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
 
 /*
@@ -88,3 +88,133 @@ if (hamburger && cola || fries === 3 && nuggets) {
 	console.log('Done!')
 }
 */
+
+// ЦИКЛЫ И МЕТКИ
+/*
+for (let i = 5; i <= 10; i++) {
+	console.log(i);
+}
+
+for (let i = 20; i > 10; i--) {
+	if (i === 13) {
+		break;
+	};
+	console.log(i);
+}
+
+for (let i = 2; i <= 10; i++) {
+	if (i % 2) {
+		continue;
+	}
+	console.log(i);
+}
+
+for (let i = 2; i <= 16; i++) {
+	if (i % 2 === 0) {
+		continue;
+	} else {
+		console.log(i);
+	}
+}
+
+let i = 2;
+
+while (i <= 16) {
+	if (i % 2) {
+		console.log(i);
+	}
+	i++;
+}
+
+const arrayOfNumbers = [];
+
+for (let i = 5; i <= 10; i++) {
+	arrayOfNumbers.push(i);
+}
+console.log(arrayOfNumbers);
+*/
+
+function firstTask() {
+	// Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+	const arr = [3, 5, 8, 16, 20, 23, 50];
+	const result = [];
+	for (let i = 0; i < arr.length; i++) {
+		const num = arr[i];
+		result.push(num);
+	}
+	// Не трогаем
+	return result;
+}
+firstTask();
+
+// Место для второй задачи
+function secondTask() {
+	// Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+	const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+	for (let i = 0; i < data.length; i++) {
+		if (typeof data[i] == 'number') {
+			data[i] = data[i] * 2;
+		} else if (typeof data[i] == 'string') {
+			data[i] = `${data[i]} - done`
+		}
+	}
+
+	// Не трогаем
+	return data;
+}
+secondTask();
+
+// Место для третьей задачи
+function thirdTask() {
+	// Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+	const data = [5, 10, 'Shopping', 20, 'Homework'];
+	const result = [];
+
+	// for (let i = data.length - 1; i !== -1; i--) {
+	// 	result.push(data[i]);
+	// }
+
+	for (let i = 1; i <= data.length; i++) {
+		result[i - 1] = data[data.length - i]
+	}
+	console.log(result);
+	// Не трогаем
+	return result;
+}
+thirdTask();
+
+function figure() {
+	const lines = 5;
+	let result = '';
+	for (let i = 0; i <= lines; i++) {
+		for (let j = 0; j <= i; j++) {
+			if (j === 0) {
+				result += `${' '.repeat(lines - i)}*`;
+			} else {
+				result += '**';
+			}
+		}
+		result += '\n';
+	}
+	// Проверяется именно переменная result, формируйте строку в ней
+	console.log(result);
+}
+figure();
+
+function figure2() {
+	const lines = 5;
+	let result = '';
+	for (let i = 0; i <= lines; i++) {
+		for (let j = 0; j < lines - i; j++) {
+			result += " ";
+		}
+		for (let j = 0; j < 2 * i + 1; j++) {
+			result += "*";
+		}
+		result += "\n";
+	}
+
+	console.log(result)
+}
+figure2();
